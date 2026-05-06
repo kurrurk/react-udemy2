cf85371178a6170fa630dea86119398b4f131190
VasilyShat <vasily.shatalkin.job@gmail.com>
2026-05-06 23:16:51 +0200

    002-Building Our First React App!

---
+++ 002-Building Our First React App!/App.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useEffect(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello World!!!</h1>
+      <button onClick={getAdvice}>Get advice</button>
+      <h2>{advice}</h2>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      You have read <strong>{props.count}</strong> piaces of advice
+    </p>
+  );
+}
